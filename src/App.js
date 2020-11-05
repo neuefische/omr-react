4b37660572e79d6bfee377aaa3814265cc8d6b9a
macfrei <lene@7freiheiten.de>
2020-11-05 15:49:24 +0100

    Add routes to render different context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import styled from 'styled-components/macro';
 import Header from './Header';
-import Page from './Page';
 import Footer from './Footer';
+import { Switch, Route } from 'react-router-dom';
 
 export default function App() {
     return (
         <AppContainer>
             <Minipage>
                 <Header headline="onemoreminiround" />
-                <Page />
+                <PageWrapper>
+                    <Switch>
+                        <Route path="/click">State</Route>
+                        <Route path="/characters">Characters</Route>
+                        <Route path="/locations">Locations</Route>
+                        <Route path="/">Home</Route>
+                    </Switch>
+                </PageWrapper>
                 <Footer />
             </Minipage>
         </AppContainer>
     );
 }
+
+const PageWrapper = styled.main`
+    background-color: #e76f51;
+`;
 
 const Minipage = styled.div`
     max-width: 470px;
